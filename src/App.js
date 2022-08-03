6e3fdb56ecbd5ce8e0662609287572292fc63e4c
riyantworked <riyantworked@gmail.com>
2022-08-03 16:39:06 +0700

    Dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/NavBar'
+import Table from './components/tableList';
+import Entry from './components/mainArea';
+import Kotakpasien from './components/kotakpasien';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mb-5">
+      <Navigation/>
+      <Table/> 
+      <Entry/>
+      <Kotakpasien/>
     </div>
   );
 }
---
+++ src/components/tableList.js
@@ -0,0 +1,54 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function Table() {
+    return (
+    <Container className='bg-white rounded-5 p-3 border border-3 shadow mb-5'>
+      <Row>
+        <Col className="square border border-1 rounded-3" style={{margin:10,padding:20}}>
+        <div>
+            <Row>
+                <Col xs={6} md={4}>
+                <img src={require('./medical-care.png')} height="70"/>
+                </Col>
+                <Col xs={12} md={8}>
+                    <h1 style={{fontSize:30}} className="justify-content-start d-flex">Kesediaan Obat</h1>
+                    <p style={{fontSize:20}} className="justify-content-start d-flex">85%</p>
+                </Col>
+            </Row>
+        </div>
+        </Col>
+        <Col className="square border border-1 rounded-3" style={{margin:10,padding:20}}>
+        <div>
+            <Row>
+                <Col xs={6} md={4}>
+                <img src={require('./medical-team.png')} height="70"/>
+                </Col>
+                <Col xs={12} md={8}>
+                    <h1 style={{fontSize:30}} className="justify-content-start d-flex">Doktor Spesialis</h1>
+                    <p style={{fontSize:20}} className="justify-content-start d-flex">21 Orang</p>
+                </Col>
+            </Row>
+        </div>
+        </Col>
+        <Col className="square border border-1 rounded-3" style={{margin:10,padding:20}}>
+        <div>
+            <Row>
+                <Col xs={6} md={4}>
+                <img src={require('./healthcare.png')} height="70"/>
+                </Col>
+                <Col xs={12} md={8}>
+                    <h1 style={{fontSize:30}} className="justify-content-start d-flex">Tenaga Medis</h1>
+                    <p style={{fontSize:20}} className="justify-content-start d-flex">40 Orang</p>
+                </Col>
+            </Row>
+        </div>
+        </Col>
+      </Row>
+    </Container>
+    );
+  }
+  
+export default Table;
